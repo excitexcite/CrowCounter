bff47ff628489e11a70b4d71ed17862a4ffd1cb6
excitexcite <excitenew@gmail.com>
2021-01-25 22:57:48 +0200

    Implement ability to count crows by clicking both crow image and calc button

--- app/src/main/java/com/peapod/crowcounter/MainActivity.java
+++ app/src/main/java/com/peapod/crowcounter/MainActivity.java
@@ -3,12 +3,25 @@ package com.peapod.crowcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView; // создали переменную, с помощью которой будем управлять выводом кол-ва ворон
+    private int crowNumber; // переменная для хранения кол-ва ворон
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.crowNumText); // связали созданную переменную с id с макета
     }
+
+    public void countCrow(View view) {
+        crowNumber++; // увеличили кол-во ворон на 1
+        textView.setText("Насчитано " + crowNumber + " ворон"); // вывели текст на экран
+    }
+
 }
